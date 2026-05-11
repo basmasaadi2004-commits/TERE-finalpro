06edc07a62d0bbd6825686dd1d51c6c46c8023ed
unknown <basmasaadi2004@gmail.com>
2026-05-11 16:28:15 +0300

    first commit

---
+++ aa.js
@@ -0,0 +1,6 @@
+document.querySelector('.btn').onclick = _ => {
+    document.querySelector('.btn').style.translate = "2rem -2rem"
+}
+document.querySelector('.help').onclick = _ => {
+    document.querySelector('.help').remove()
+}
